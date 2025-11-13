272796688eac9be03f3ce024d68ff38948abbcb2
R00neyj <yjw1555@gmail.com>
2025-11-14 02:02:03 +0900

    sec2 working

--- javascript/script.js
+++ javascript/script.js
@@ -3,10 +3,10 @@ new Swiper(".hero-swiper", {
   parallax: true,
   speed: 800,
 
-  // autoplay: {
-  //   delay: 5000,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     type: "fraction",
@@ -25,3 +25,28 @@ new Swiper(".hero-swiper", {
     },
   },
 });
+
+const toolTipBox = document.querySelector(".tooltip-box");
+const toolTipText = toolTipBox.querySelector(".tipText");
+const hoverEl = document.querySelectorAll(`[data-hover="true"]`);
+
+document.addEventListener("mousemove", (eh) => {
+  let mouseX = eh.clientX;
+  let mouseY = eh.clientY;
+  toolTipBox.style.transform = `translate3d(${mouseX + 10}px, calc(${mouseY}px + 2.4rem), 0)`;
+});
+
+hoverEl.forEach((el) => {
+  let text;
+
+  el.addEventListener("mouseenter", (e) => {
+    text = el.getAttribute("data-hover-text");
+    toolTipText.textContent = text;
+    console.log(text);
+    toolTipBox.classList.add("active");
+  });
+  el.addEventListener("mouseleave", (e) => {
+    toolTipBox.classList.remove("active");
+  });
+});
+// document.querySelector(".sec-1 .text-box").innerHTML = `<h2>임시텍스트</h2><p>임시</p>`;
