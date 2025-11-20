380173b61f9dd89bae65130c4fd2f02a5d5d43a9
R00neyj <yjw1555@gmail.com>
2025-11-21 01:52:37 +0900

    PC finished

--- javascript/script.js
+++ javascript/script.js
@@ -99,10 +99,6 @@ function sec_1_gsap__init() {
 
   function horizontalScroll() {
     const totalScrollDistance = imgContainerWrap.offsetWidth - window.innerWidth;
-    const imgBoxCenterOffset = lastImgBox.offsetLeft + lastImgBox.offsetWidth / 2 - window.innerWidth / 2;
-    const startProgress = imgBoxCenterOffset / totalScrollDistance;
-
-    console.log(startProgress);
 
     let tl = gsap.timeline();
     tl.to(imgContainerWrap, {
@@ -119,19 +115,15 @@ function sec_1_gsap__init() {
       "-=0.5"
     );
     tl.to(".sec-1 .img-box:nth-child(6) img", { filter: "brightness(0.6)", ease: "none" }, "<");
-    tl.to(
-      lastImgBox,
-      {
-        width: "100vw",
-        right: 0,
-        x: 0,
-        height: "100dvh",
-        duration: 2,
-        ease: "none",
-      },
-      "+=0.5"
-    );
-    tl.to({}, { duration: 1 });
+    tl.to(lastImgBox, {
+      width: "100vw",
+      right: 0,
+      x: 0,
+      height: "100dvh",
+      duration: 1,
+      ease: "none",
+    });
+    tl.to({}, { duration: 0.1 });
 
     let st = ScrollTrigger.create({
       trigger: sec_1_pin,
@@ -147,10 +139,10 @@ function sec_1_gsap__init() {
     let tlText = gsap.timeline();
     tlText
       .to(textBox[0], { opacity: 1, duration: 0, ease: "none" })
-      .to(textBox, { duration: 0.5 })
+      .to({}, { duration: 3 })
       .to(textBox[0], { opacity: 0, ease: "none" })
       .to(textBox[1], { opacity: 1, ease: "none" })
-      .to(textBox, { duration: 1 })
+      .to({}, { duration: 3 })
       .to(textBox[1], { opacity: 0, duration: 1, ease: "none" })
       .to(textBox[2], { opacity: 1, ease: "none" });
     tlText.to({}, { duration: 3 });
@@ -170,17 +162,26 @@ function sec_2_gsap__init() {
 
   cards.forEach((card, index) => {
     const target = card.querySelector("a");
-    const prevTarget = index >= 1 ? cards[index - 1].querySelector("a") : null;
+    const h3 = target.querySelector("h3");
+    const p = target.querySelector("p");
 
-    const h3 = target.querySelector(".text-box h3");
-    const p = target.querySelector(".text-box p");
+    let prevTarget = null,
+      pervH3 = null,
+      prevP = null;
+    if (index >= 1) {
+      prevTarget = cards[index - 1].querySelector("a");
+      pervH3 = prevTarget.querySelector("h3");
+      prevP = prevTarget.querySelector("p");
+    }
 
     let tl = gsap.timeline();
     tl.add("start");
-    tl.to(target, { width: "133rem", height: "100%", duration: 1, ease: "none" });
-    tl.to(prevTarget, { width: "80rem", height: "50%", duration: 1, ease: "none" }, "<10%");
-    tl.to(h3, { fontSize: "4.8rem", ease: "none" }, "start");
-    tl.to(p, { fontSize: "3.2rem", ease: "none" }, "start");
+    tl.to(target, { width: "133rem", height: "100%", duration: 1 });
+    tl.to(prevTarget, { width: "80rem", height: "50%", duration: 1 }, "<10%");
+    tl.to(pervH3, { fontSize: "3.2rem" }, "<");
+    tl.to(prevP, { fontSize: "2rem" }, "<");
+    tl.to(h3, { fontSize: "4.8rem" }, "start");
+    tl.to(p, { fontSize: "3.2rem" }, "start");
 
     let st = ScrollTrigger.create({
       trigger: card,
@@ -227,12 +228,13 @@ function sec_3_gsap__init() {
     const path = sec3.querySelector("#sec-3-path");
     const circle = sec3.querySelector("#sec-3-circle");
 
-    let duration = 3.3;
+    let duration = 2.5;
     let ease = "power2.out";
 
     let tl = gsap.timeline();
     tl.set(circle, { opacity: 0 });
-    tl.from(path, { drawSVG: "0", ease: ease });
+    tl.from(path, { drawSVG: "0", duration: duration, ease: ease });
+    tl.to(circle, { opacity: 1, ease: ease }, "<");
     tl.to(
       circle,
       {
@@ -243,7 +245,7 @@ function sec_3_gsap__init() {
           start: 0,
           end: 1,
         },
-        opacity: 1,
+        duration: duration,
         ease: ease,
       },
       "<"
@@ -252,9 +254,12 @@ function sec_3_gsap__init() {
     let st = ScrollTrigger.create({
       trigger: head,
       animation: tl,
+      pin: true,
+      start: "top top",
+      end: "+=200%",
       scrub: duration,
-      start: "25% center",
-      end: "center center",
+
+      // toggleActions: "play none none reset",
     });
   }
 }
@@ -284,6 +289,52 @@ function headerST() {
   });
 }
 
+function animation__init() {
+  const target = document.querySelectorAll("[data-ani]");
+  let defalutDuration = 0.6;
+
+  target.forEach((el) => {
+    let aniType = el.getAttribute("data-ani");
+    let delay = el.getAttribute("data-delay") == null ? 0 : el.getAttribute("data-delay");
+    let duration = el.getAttribute("data-duration") == null ? defalutDuration : el.getAttribute("data-duration");
+    console.log(duration);
+
+    if (aniType == "up") {
+      aniUp(el, delay, duration);
+    } else if (aniType == "right") {
+      aniRight(el, delay, duration);
+    } else {
+      return false;
+    }
+  });
+
+  function aniUp(El, de, du) {
+    gsap.set(El, { opacity: 0 });
+    let tl = gsap.timeline();
+    tl.to(El, { y: "50%", opacity: 0, duration: 0, ease: "none" });
+    tl.to(El, { y: 0, opacity: 1, duration: du, ease: "power2.out", delay: de });
+    let st = ScrollTrigger.create({
+      trigger: El,
+      animation: tl,
+      toggleActions: "play none none reverse",
+      start: "top 90%",
+    });
+  }
+  function aniRight(El, de, du) {
+    gsap.set(El, { opacity: 0 });
+    let tl = gsap.timeline();
+    tl.to(El, { x: "-50%", opacity: 0, duration: 0, ease: "none" });
+    tl.to(El, { x: 0, opacity: 1, duration: du, ease: "power2.out", delay: de });
+
+    let st = ScrollTrigger.create({
+      trigger: El,
+      animation: tl,
+      toggleActions: "play none none reverse",
+      start: "top 90%",
+    });
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   swiper__init();
   tooltip();
@@ -291,6 +342,7 @@ document.addEventListener("DOMContentLoaded", () => {
   sec_2_gsap__init();
   sec_3_gsap__init();
   headerST();
+  animation__init();
 
   const allLinks = document.querySelectorAll("a");
 
